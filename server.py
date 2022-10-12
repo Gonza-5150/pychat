cbbcb9d2b477de30adac6fde3489a2dfdd7a5618
Gonza-5150 <estevesgonzalo@hotmail.com>
2022-10-12 18:58:11 +0000

    server2

--- server.py
+++ server.py
@@ -6,7 +6,7 @@ port = 55555
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-server.bind((host, post))
+server.bind((host, port))
 server.listen()
 print(f"server running on {host}:{port}")
 
